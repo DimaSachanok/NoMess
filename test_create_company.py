b46cd7a62af10e7df90c55011374efd28c967254
Dzmitry Sachanok <sachenokdima1995@gmail.com>
2023-09-13 17:32:17 +0200

    other

---
+++ test_create_company.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+from time import sleep
+import pytest
+
+
+@pytest.fixture
+def my_fixture():
+    pass
+
+
+def test_login(my_fixture):
+    pass
+
