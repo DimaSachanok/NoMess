be534e77c218f188e0106574e92160a17c0e714d
Dzmitry Sachanok <sachenokdima1995@gmail.com>
2023-09-13 16:45:49 +0200

    aded fixture

--- test_login.py
+++ test_login.py
@@ -1,5 +1,15 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 from time import sleep
+import pytest
+
+
+@pytest.fixture
+def my_fixture():
+    pass
+
+
+def test_login(my_fixture):
+    pass
 
 
 def test_login(playwright: Playwright) -> None:
@@ -7,7 +17,8 @@ def test_login(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
     page.goto("http://109.205.183.105/")
-    page.goto("http://109.205.183.105:8080/realms/captain/protocol/openid-connect/auth?client_id=captain-fe&redirect_uri=http%3A%2F%2F109.205.183.105%2F&state=a759ebdf-a18f-42fc-8669-09472396830a&response_mode=fragment&response_type=code&scope=openid&nonce=bdbd4121-7fdd-4b15-8fc9-3c9dd970c6ca")
+    page.goto(
+        "http://109.205.183.105:8080/realms/captain/protocol/openid-connect/auth?client_id=captain-fe&redirect_uri=http%3A%2F%2F109.205.183.105%2F&state=a759ebdf-a18f-42fc-8669-09472396830a&response_mode=fragment&response_type=code&scope=openid&nonce=bdbd4121-7fdd-4b15-8fc9-3c9dd970c6ca")
     page.get_by_label("Username or email").click()
     sleep(1.5)
     page.get_by_label("Username or email").fill("default.admin@tect.com")
